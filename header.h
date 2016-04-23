a7f300e34792e8a11956dd7d47d27a631404b188
nephill <ljnsjtu@hotmail.com>
2016-04-23 20:23:19 +0800

    rename head.h->header.h

--- header.h
+++ header.h
@@ -16,13 +16,13 @@
 #include <unordered_map>
 #include <iostream>
 #include <errno.h>
+#include "linear.h"
 
 #define Malloc(type,n) (type *)malloc((n)*sizeof(type))
 #define INF HUGE_VAL
 
 typedef long unsigned int size_type;
 
-char* transformLabel(char*);
 void parse_command_line(int, char **, char *input_file_name, char *model_file_name);
 void read_problem(const char *filename);
 void print_null(const char *s);
@@ -35,6 +35,11 @@ void priori_min_max_train(int, char**);
 void __priori_min_max_train(char*);
 void min_max_train(int argc, char** argv);
 void __min_max_train(char* test_file_name);
+void naive_train(int, char**);
+void __naive_train(char*);
+std::string transformLabel(std::string);
+std::vector<std::string> split(const std::string&, char);
+std::string changeFileName(const std::string&);
 
 struct feature_node *x_space;
 struct parameter param={};
